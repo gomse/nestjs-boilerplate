b2013abc475468b389ed805937c498fe15912f9d
sehwan <sehwan@liche.co.kr>
2023-07-20 01:44:58 +0900

    Set global prefix `/v1`

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
+  app.setGlobalPrefix('v1');
   await app.listen(19300);
 }
 
