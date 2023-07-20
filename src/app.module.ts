a9ced4a5f0636bdb77b45bf91ae9d6dd47dbbab7
sehwan <sehwan@liche.co.kr>
2023-07-20 10:42:58 +0900

    Add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { GlobalModule } from './global/global.module';
 import { HelloModule } from './hello/hello.module';
 
 @Module({
-  imports: [GlobalModule, HelloModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GlobalModule,
+    HelloModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,41 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  const isProduction = config.get<string>('NODE_ENV') === 'production';
+  const port = config.get<number>('PORT');
+
   app.enableShutdownHooks();
-  app.setGlobalPrefix('v1');
-  await app.listen(19300);
+
+  // Cross-Origin Resource Sharing
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  });
+
+  // Versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  // class-validator & class-transformer
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      disableErrorMessages: isProduction,
+    }),
+  );
+
+  await app.listen(port);
 }
 
 bootstrap();
